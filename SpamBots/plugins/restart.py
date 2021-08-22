11dec158b916df7692355d29ea79f5e8e063123b
dangerousjatt <73658726+dangerousjatt@users.noreply.github.com>
2021-08-22 07:59:30 +0530

    Update restart.py

--- SpamBots/plugins/restart.py
+++ SpamBots/plugins/restart.py
@@ -5,7 +5,36 @@ SMEX_USERS = []
 for x in SUDO_USERS:
     SMEX_USERS.append(x)
 
-
+@UstaD.on(
+    events.NewMessage(pattern="^/restart", func=lambda e: e.sender_id in SMEX_USERS)
+)
+@UstaD2.on(
+    events.NewMessage(pattern="^/restart", func=lambda e: e.sender_id in SMEX_USERS)
+)
+@UstaD3.on(
+    events.NewMessage(pattern="^/restart", func=lambda e: e.sender_id in SMEX_USERS)
+)
+@UstaD4.on(
+    events.NewMessage(pattern="^/restart", func=lambda e: e.sender_id in SMEX_USERS)
+)
+@UstaD5.on(
+    events.NewMessage(pattern="^/restart", func=lambda e: e.sender_id in SMEX_USERS)
+)
+@UstaD6.on(
+    events.NewMessage(pattern="^/restart", func=lambda e: e.sender_id in SMEX_USERS)
+)
+@UstaD7.on(
+    events.NewMessage(pattern="^/restart", func=lambda e: e.sender_id in SMEX_USERS)
+)
+@UstaD8.on(
+    events.NewMessage(pattern="^/restart", func=lambda e: e.sender_id in SMEX_USERS)
+)
+@UstaD9.on(
+    events.NewMessage(pattern="^/restart", func=lambda e: e.sender_id in SMEX_USERS)
+)
+@UstaD10.on(
+    events.NewMessage(pattern="^/restart", func=lambda e: e.sender_id in SMEX_USERS)
+)
 async def restart(e):
     if e.sender_id in SMEX_USERS:
         text = "𝐑𝐄𝐒𝐓𝐀𝐑𝐓𝐄𝐃\n\𝐏𝐋𝐄𝐀𝐒𝐄 𝐖𝐀𝐈𝐓 𝐓𝐈𝐋𝐋 𝐈𝐓 𝐑𝐄𝐁𝐎𝐎𝐓𝐒...."
