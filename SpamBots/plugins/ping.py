5dde1de6c39088a95a496461fad048c108f2fd32
DEADLY-FIGHTERS <87220607+DEADLY-FIGHTERS@users.noreply.github.com>
2021-08-29 14:43:05 +0530

    Update ping.py

--- SpamBots/plugins/ping.py
+++ SpamBots/plugins/ping.py
@@ -53,4 +53,4 @@ async def ping(e):
         event = await e.reply(text, parse_mode=None, link_preview=None )
         end = datetime.now()
         ms = (end-start).microseconds / 1000
-        await event.edit(f"🤖 𝗣𝗼𝗻𝗴!\n`{ms}` 𝗺𝘀")        
+        await event.edit(f"🤖 🇵 🇴 🇳 🇬 !\n`{ms}` 𝗺𝘀")                       
