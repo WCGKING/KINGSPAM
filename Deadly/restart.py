a3f793155b44f70f1831b22996ed57305163f4ca
Ustad <73658726+Ustad-Op@users.noreply.github.com>
2021-09-02 14:50:55 +0530

    Update restart.py

--- Deadly/restart.py
+++ Deadly/restart.py
@@ -13,13 +13,13 @@ if UstaD:
     async def restart(e):
         if e.sender_id in SMEX_USERS:
              text = " 🤖𝐑𝐄𝐒𝐓𝐀𝐑𝐓𝐄𝐃🤖\n🔰𝐏𝐋𝐄𝐀𝐒𝐄 𝐖𝐀𝐈𝐓 𝐓𝐈𝐋𝐋 𝐈𝐓 𝐑𝐄𝐁𝐎𝐎𝐓𝐒...."
-            await e.reply(text, parse_mode=None, link_preview=None)
-       try:
-            await UstaD.disconnect()
-       except Exception:
+             await e.reply(text, parse_mode=None, link_preview=None)
+             try:
+                await UstaD.disconnect()
+             except Exception:
             pass
-       os.execl(sys.executable, sys.executable, *sys.argv)
-       quit()
+            os.execl(sys.executable, sys.executable, *sys.argv)
+            quit()
 
 else:
    pass
