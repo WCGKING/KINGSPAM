f841c35d50c6ab69221e263602826b93465179a5
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-01-30 20:22:16 +0530

    Omk

--- OfficialSameer/utils.py
+++ OfficialSameer/utils.py
@@ -8,14 +8,14 @@ import inspect
 import re
 
 def load_plugins(plugin_name):
-    path = Path(f"OfficialSameer/plugins/{plugin_name}.py")
-    name = "OfficialSameer.plugins.{}".format(plugin_name)
+    path = Path(f"Godfather/plugins/{plugin_name}.py")
+    name = "Godfather.plugins.{}".format(plugin_name)
     spec = importlib.util.spec_from_file_location(name, path)
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
-    sys.modules["OfficialSameer.plugins." + plugin_name] = load
-    print("Deadly Spam Bot has Imported " + plugin_name)
+    sys.modules["Godfather.plugins." + plugin_name] = load
+    print("Godfather Imported Successfully 🎉 " + plugin_name)
 
 async def edit_or_reply(event, text):
     if event.sender_id in SUDO_USERS:
