df3bb12cbc412e0b34c350ab6b20de8d118df705
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-01-31 09:27:55 +0530

    Umm fixed

--- Godfather/plugins/leave.py
+++ Godfather/plugins/leave.py
@@ -1,5 +1,5 @@
 import asyncio
-from Godfather import BOT0, BOT1, BOT2, BOT3, BOT4, BOT5, BOT6, BOT7, BOT8, BOT9
+from Godfather import BOT0, BOT1, BOT2, BOT3, BOT4, BOT5, BOT6, BOT7, BOT8, BOT9, SUDO_USERS, OWNER_ID
 from Godfather import CMD_HNDLR as hl
 import telethon.utils
 from telethon.tl import functions
