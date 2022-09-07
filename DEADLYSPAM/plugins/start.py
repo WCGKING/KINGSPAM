b007dc46823568574b3cd2ee01cc0b0221a75081
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-09-07 08:36:06 +0530

    Done

--- DEADLYSPAM/plugins/start.py
+++ DEADLYSPAM/plugins/start.py
@@ -1,23 +1,14 @@
 
 import asyncio
 import os
-from telethon.tl.functions.users import GetFullUserRequest
 from telethon import events, Button
 from telethon.tl.custom import button
 from DEADLYSPAM import BOT0, BOT1, BOT2, BOT3, BOT4, BOT5, BOT6, BOT7, BOT8, BOT9, ALIVE_PIC, OWNER_ID
 
 DEADLY_IMG = ALIVE_PIC if ALIVE_PIC else "https://telegra.ph/file/c6f99c0b68ff07439ed72.jpg"
 
+
 Deadly_Button = [
-        [
-        Button.url("• Sᴜᴘᴘᴏʀᴛ •", "https://t.me/TheDeadlyBots")
-        ],
-        [
-        Button.url("• Mᴀɪɴᴛᴀɪɴ Bʏ •", "https://t.me/Deadly_spambot")
-        ]
-        ]
-               
-DeadlyX_Button = [
         [
         Button.url("Cʜᴀɴɴᴇʟ", "https://t.me/Deadly_spambot"),
         Button.url("Sᴜᴘᴘᴏʀᴛ", "https://t.me/TheDeadlyBots")
@@ -27,38 +18,40 @@ DeadlyX_Button = [
         ]
         ]
         
-        
+
 #USERS 
 
 
-@BOT0.on(events.NewMessage(pattern="/start"))
-@BOT1.on(events.NewMessage(pattern="/start"))
-@BOT2.on(events.NewMessage(pattern="/start"))
-@BOT3.on(events.NewMessage(pattern="/start"))
-@BOT4.on(events.NewMessage(pattern="/start"))
-@BOT5.on(events.NewMessage(pattern="/start"))
-@BOT6.on(events.NewMessage(pattern="/start"))
-@BOT7.on(events.NewMessage(pattern="/start"))
-@BOT8.on(events.NewMessage(pattern="/start"))
-@BOT9.on(events.NewMessage(pattern="/start"))
-async def start(event):              
-    if event.is_private:
-       DeadlyBot = await event.client.get_me()
-       bot_id = DeadlyBot.first_name
-       bot_username = DeadlyBot.username
-       replied_user = await event.client(GetFullUserRequest(event.sender_id))
-       TheDeadly = event.chat_id
-       firstname = replied_user.user.first_name
-       ownermsg = f"**Hi Master, Its me {bot_id}, Your Spam Bot !! \n\n Click Below Buttons For help**"
-       usermsg = f"**Hello, {firstname} ! Nice To Meet You, Well I Am {bot_id}, An Powerfull Spam Bot.** \n\n**If You Want Your Own Spam Bots You Can Deploy From Button Below.** \n\n**"
-       if event.sender_id == OWNER_ID:
-            await event.client.send_file(TheDeadly,
-                  DEADLY_IMG,
-                  caption=ownermsg, 
-                  buttons=Deadly_Button)
-       else:
-            await event.client.send_file(TheDeadly,
-                  DEADLY_IMG,
-                  caption=usermsg, 
-                  buttons=DeadlyX_Button)
-                
+@BOT0.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT1.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT2.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT3.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT4.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT5.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT6.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT7.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT8.on(events.NewMessage(incoming=True, pattern='/start'))
+@BOT9.on(events.NewMessage(incoming=True, pattern='/start'))
+async def start(e):
+    if e.chat_id is e.sender_id:
+        name = e.sender.first_name
+        user_id = e.sender_id
+        mention = f"[{name}](tg://user?id={user_id})"
+        myOwner = f"[{OWNER_NAME}](tg://user?id={OWNER_ID})"
+        creator = f"[Blaze](tg://user?id={5256676062})"
+        sudo_user = ""
+        if e.sender_id in OWNER_ID:
+            sudo_user = "True"
+        else:
+            sudo_user = "False"
+        DEADLY_ON = f"""
+Hey {mention},
+This Is Deadly Spam Bot!
+
+Owner:- {myOwner}
+OWNER:- {sudo_user}
+Creator:- {creator}
+
+Click Below Button For Detailed informations!
+    """
+        await e.client.send_file(e.chat_id, DEADLY_IMG, caption=DEADLY_ON, buttons=Deadly_Button)
