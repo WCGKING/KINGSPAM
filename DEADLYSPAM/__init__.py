5ca527b20c90f4c7754d76050a6657a4a93ba636
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-10-31 20:05:10 +0530

    Update __init__.py

--- DEADLYSPAM/__init__.py
+++ DEADLYSPAM/__init__.py
@@ -16,7 +16,7 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 #version
 
-deadlyversion = "v0.3.0"
+deadlyversion = "v0.3.1"
 
 #values
 API_ID = config("API_ID", default=None, cast=int)
