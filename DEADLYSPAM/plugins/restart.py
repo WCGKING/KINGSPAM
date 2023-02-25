a3cd4aea239957ec0c57f6d86911ea8c6a29cd1b
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2023-02-26 00:21:08 +0530

    Create restart.py

---
+++ DEADLYSPAM/plugins/restart.py
@@ -0,0 +1,63 @@
+import os
+from DEADLYSPAM import BOT0, BOT1, BOT2, BOT3, BOT4, BOT5, BOT6, BOT7, BOT8, BOT9, SUDOERS, CMD_HNDLR as hl
+from telethon.tl.functions.channels import LeaveChannelRequest
+from telethon import events
+
+@BOT0.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
+@BOT1.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
+@BOT2.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
+@BOT3.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
+@BOT4.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
+@BOT5.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
+@BOT6.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
+@BOT7.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
+@BOT8.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
+@BOT9.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
+async def restart(event):
+    if event.sender_id in SUDOERS:
+        await event.reply(f"**[ᴅᴇᴀᴅʟʏ-ꜱᴘᴀᴍʙᴏᴛ 🔥](https://t.me/deadly_spam_bot) : ʀᴇꜱᴛᴀʀᴛɪɴɢ!")
+        try:
+            await BOT0.disconnect()
+        except Exception:
+            pass
+        try:
+            await BOT1.disconnect()
+        except Exception:
+            pass
+        try:
+            await BOT2.disconnect()
+        except Exception:
+            pass
+        try:
+            await BOT3.disconnect()
+        except Exception:
+            pass
+        try:
+            await BOT4.disconnect()
+        except Exception:
+            pass
+        try:
+            await BOT5.disconnect()
+        except Exception:
+            pass
+        try:
+            await BOT6.disconnect()
+        except Exception:
+            pass
+        try:
+            await BOT7.disconnect()
+        except Exception:
+            pass
+        try:
+            await BOT8.disconnect()
+        except Exception:
+            pass
+        try:
+            await BOT9.disconnect()
+        except Exception:
+            pass
+
+        os.execl(sys.executable, sys.executable, *sys.argv)
+        quit()
+        
+
