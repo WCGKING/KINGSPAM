9dd12c8e93f476984e697bc0f464e38543323d4d
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2023-02-26 08:45:25 +0530

    Update spam.py

--- DEADLYSPAM/plugins/spam.py
+++ DEADLYSPAM/plugins/spam.py
@@ -181,7 +181,7 @@ async def pspam(e):
         if len(Deadly) == 1:
             counter = int(Deadly[0])
             if int(e.chat_id) in GROUP:
-                text = f"Sorry !! I can't spam here"
+                text = f"ꜱᴘᴀᴍᴍɪɴɢ ɪɴ ᴅᴇᴀᴅʟʏ ᴄʜᴀᴛꜱ ɪꜱ ɪʟʟɪɢᴀʟ"
                 await e.reply(text)
             else:
                  porrn = random.choice(PORMS)
