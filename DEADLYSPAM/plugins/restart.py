ec6b5dff5dc95a3877c0d4f4737ec07dd6d66efc
MR AGORA <106959345+MR-AGORA@users.noreply.github.com>
2023-03-19 05:31:09 +0530

    Update restart.py

--- DEADLYSPAM/plugins/restart.py
+++ DEADLYSPAM/plugins/restart.py
@@ -20,7 +20,7 @@ hl = config.CMD_HNDLR
 async def restart(event):
     await event.delete() 
     if event.sender_id in SUDOERS:
-        await event.reply(f"**ᴅᴇᴀᴅʟʏ-ꜱᴘᴀᴍʙᴏᴛ 🔥: ʀᴇꜱᴛᴀʀᴛɪɴɢ!**")
+        await event.reply(f"**ᴢᴀʀᴀ ʀᴜᴋᴋᴏ ʙʜᴀɪ 🔥: ʀᴇᴅ ʙᴜʟʟ ᴘɪ ʀᴀʜ ʜᴜ!**")
         try:
             await BOT0.disconnect()
         except Exception:
