8225b1d4b98135cf6c908a1725c73259de95d0cf
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-03-27 20:46:04 +0530

    Update echo.py

--- DEADLYSPAM/plugins/echo.py
+++ DEADLYSPAM/plugins/echo.py
@@ -28,7 +28,7 @@ async def echo(event):
             reply_msg = await event.get_reply_message()
             user_id = reply_msg.sender_id
             if int(user_id) in DEADLYSPAM:
-                    text = "**ᴄᴀɴɴᴏᴛ ᴇᴄʜᴏ ᴏɴ ғᴄʀ x ᴛᴇᴀᴍ ᴀɢᴏʀᴀ -ꜱᴘᴀᴍʙᴏᴛ ᴅᴇᴠᴇʟᴏᴘᴇʀꜱ !**"
+                    text = "**ᴄᴀɴɴᴏᴛ ᴇᴄʜᴏ ᴏɴ ᴋɪɴɢ x ᴛᴇᴀᴍ ʙʀᴀɴᴅᴇᴅ -ꜱᴘᴀᴍʙᴏᴛ ᴅᴇᴠᴇʟᴏᴘᴇʀꜱ !**"
                     await event.reply(text, parse_mode=None, link_preview=None )
             elif int(user_id) == config.OWNER_ID:
                     text = f"ᴄᴀɴɴᴏᴛ ᴇᴄʜᴏ ᴏɴ ᴏᴡɴᴇʀ !"
