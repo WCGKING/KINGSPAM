06ebb64fc7ac92befcd70e0c8f5374fc9eaf523b
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-03-27 19:38:04 +0530

    Update __main__.py

--- DEADLYSPAM/__main__.py
+++ DEADLYSPAM/__main__.py
@@ -20,7 +20,7 @@ for name in files:
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
 
-deadly = "🎉 Successfully Deployed TAXFCR SpamBot 🎉 @FCR_TEAM_AGORA_FIGHTER Enjoy! Do visit @DO_JISM_EK_JAAN_OP"
+deadly = "💙👀 Successfully Deployed TAXFCR SpamBot 💙👀 @BRANDED_WORLD Enjoy! Do visit @BRANDED_KHUSHI"
 print(deadly[0: ])
 
 if __name__ == "__main__":
